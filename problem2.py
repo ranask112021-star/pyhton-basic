0b2b8b88acf10e27625132473e84280b0dedf16a
ranask112021-star <ranask112021@gmail.com>
2025-12-19 10:17:28 +0530

    Change speech text in problem2.py

--- problem2.py
+++ problem2.py
@@ -1,6 +1,6 @@
 #install an  external module  and use it to perform an operation of  your interest.
 import pyttsx3
 engine = pyttsx3.init()
-engine.say("kya tum isko jante ho ye bahut AChi hai")
+engine.say("do you Know my Friends Name")
 
 engine.runAndWait()
